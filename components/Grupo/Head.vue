62dcf1e6fd72ded9d174fcf4b71fded1c4a163c4
aleDonatto <ale_donatto@yahoo.com.mx>
2022-07-19 12:30:09 -0500

    grupo polnac

--- components/Grupo/Head.vue
+++ components/Grupo/Head.vue
@@ -2,10 +2,10 @@
     <div class="bg-grupo">
         <v-row justify="center">
             <v-col cols="8" align="center" class="mt-16">
-                <h1 class="mt-5 font-title text-h3 white--text font-weight-bold">Grupo POLNAC</h1>
+                <h1 class="mt-5 font-archivo text-h3 white--text font-weight-bold">Grupo POLNAC</h1>
 
                 <p class="title white--text mt-8 mb-16">
-                    Desde 1984, la familia <span class="yellow--text">POLNAC</span> ha acrecentado sus miembros. <br>
+                    Desde 1984, la familia <span class="yellow--text font-weight-bold">POLNAC</span> ha acrecentado sus miembros. <br>
                     Hoy en día son tres empresas y 8 marcas las cuales forman parte del grupo líder en soluciones de la industria plástica.
                 </p>
             </v-col>
@@ -20,7 +20,6 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Archivo:wght@300&display=swap');
 
 .bg-grupo{
     background-image: linear-gradient(to right, rgba(25, 211, 197, 0.7),rgba(61, 124, 201, 0.7)), url('../../static/group/bg-grupo-polnac.png');
@@ -36,7 +35,4 @@ export default {
     height: 540px;
 }
 
-.font-title{
-    font-family: 'Archivo' !important;
-}
 </style>
--- components/Grupo/Sections.vue
+++ components/Grupo/Sections.vue
@@ -4,8 +4,8 @@
             <v-container>
                 <v-row justify="center" class="my-16">
                     <v-col cols="12" lg="6" md="6" sm="12" xs="12" align-self="center">
-                        <p class="text-body-all">Sin importar cuales sean tus necesidades <span class="font-weight-bold">Grupo POLNAC</span> tiene soluciones para ti: </p>
-                        <ul class="text-body-all">
+                        <p class="font-size-20">Sin importar cuales sean tus necesidades <span class="font-weight-bold">Grupo POLNAC</span> tiene soluciones para ti: </p>
+                        <ul class="font-size-20">
                             <li>Resinas</li>
                             <li>Compuestos</li>
                             <li>Concentrados de Color</li>
@@ -13,7 +13,7 @@
                             <li>Reciclados y resinas Post-Industriales</li>
                             <li>Servicios de logística y mucho más</li>
                         </ul>
-                        <p class="text-body-all mt-5">
+                        <p class="font-size-20 mt-5">
                             La filosofía POLNAC respalda a todo el grupo: proporcionar soluciones innovadoras con el mejor servicio, la más alta calidad y a precios altamente competitivos.
                         </p>
                     </v-col>
@@ -28,13 +28,13 @@
             <v-container>
                 <v-row class="" justify="center">
                     <v-col align="center">
-                        <h1 class="font-title text-h3 font-weight-bold my-10">Empresas filiales</h1>
+                        <h1 class="font-archivo text-h3 font-weight-bold my-10">Empresas filiales</h1>
                     </v-col>
                 </v-row>
                 <v-row justify="center" class="my-16">
                     <v-col cols="12" lg="6" md="6" sm="12" xs="12" align="center">
                         <v-img src="/group/logo-poliresin.png" contain max-height="150"></v-img>
-                        <v-btn rounded outlined color="teal" class="mt-4 px-10 py-6 text-none">
+                        <v-btn rounded outlined color="#19D3C5" class="mt-4 px-10 py-6 text-none">
                             <nuxt-link to="/poliresin" class="text-decoration-none black--text">
                                 Saber más
                             </nuxt-link>
@@ -42,7 +42,7 @@
                     </v-col>
                     <v-col cols="12" lg="6" md="6" sm="12" xs="12" align="center">
                         <v-img src="/group/logo-transpolimer.png" contain max-height="150"></v-img>
-                        <v-btn rounded outlined color="teal" class="mt-4 px-10 py-6 text-none">
+                        <v-btn rounded outlined color="#19D3C5" class="mt-4 px-10 py-6 text-none">
                             <nuxt-link to="/transpolimer" class="text-decoration-none black--text">Saber más</nuxt-link>
                         </v-btn>
                     </v-col>
@@ -54,42 +54,42 @@
             <v-container>
                 <v-row>
                     <v-col align="center" class="my-10">
-                        <h1 class="font-title text-h3 font-weight-bold">Nuestras marcas</h1>
+                        <h1 class="font-archivo text-h3 font-weight-bold">Nuestras marcas</h1>
                     </v-col>
                 </v-row>
                 <v-row justify="center" class="my-16"> 
                     <v-col cols="12" lg="3" md="3" sm="12" xs="12" align="center">
                         <v-img src="/group/logo-colorforte.png" contain max-height="90"></v-img>
-                        <p class="body-1 text-left mt-5">Innovación en el desarrollo, diseño y fabricación de...</p>
+                        <p class="font-size-18 text-left mt-5">Innovación en el desarrollo, diseño y fabricación de...</p>
                     </v-col>
                     <v-col cols="12" lg="3" md="3" sm="12" xs="12" align="center">
                         <v-img src="/group/logo-poliforte.png" contain max-height="90"></v-img>
-                        <p class="body-1 text-left mt-5">Líder en el diseño, fabricación y comercialización de Compuestos.</p>
+                        <p class="font-size-18 text-left mt-5">Líder en el diseño, fabricación y comercialización de Compuestos.</p>
                     </v-col>
                     <v-col cols="12" lg="3" md="3" sm="12" xs="12" align="center">
                         <v-img src="/group/logo-bioresin.png" contain max-height="90"></v-img>
-                        <p class="body-1 text-left mt-5">Especialistas en la distribución y fabricación de resinas biodegradables. </p>
+                        <p class="font-size-18 text-left mt-5">Especialistas en la distribución y fabricación de resinas biodegradables. </p>
                     </v-col>
                     <v-col cols="12" lg="3" md="3" sm="12" xs="12" align="center">
                         <v-img src="/group/logo-rototech.png" contain max-height="90"></v-img>
-                        <p class="body-1 text-left mt-5">La línea de negocio especializada en la producción y comercialización de resinas plásticas para el proceso de rotomoldeo.</p>
+                        <p class="font-size-18 text-left mt-5">La línea de negocio especializada en la producción y comercialización de resinas plásticas para el proceso de rotomoldeo.</p>
                     </v-col>
 
                     <v-col cols="12" lg="3" md="3" sm="12" xs="12">
                         <v-img src="/group/logo-gomalene.png" contain max-height="90"></v-img>
-                        <p class="body-1 text-left mt-5">El portafolio más completo de Elastómeros Termoplásticos y Hules:</p>
+                        <p class="font-size-18 text-left mt-5">El portafolio más completo de Elastómeros Termoplásticos y Hules:</p>
                     </v-col>
                     <v-col cols="12" lg="3" md="3" sm="12" xs="12">
                         <v-img src="/group/logo-flexlene.png" contain max-height="90"></v-img>
-                        <p class="body-1 text-left mt-5">Expertos en el diseño, desarrollo y fabricación de Compuestos de Policloruro de Vinilo (PVC).</p>
+                        <p class="font-size-18 text-left mt-5">Expertos en el diseño, desarrollo y fabricación de Compuestos de Policloruro de Vinilo (PVC).</p>
                     </v-col>
                     <v-col cols="12" lg="3" md="3" sm="12" xs="12">
                         <v-img src="/group/logo-politene.png" contain max-height="90"></v-img>
-                        <p class="body-1 text-left mt-5">Las mejores resinas a tu disposición. Los productos más destacados de los fabricantes</p>
+                        <p class="font-size-18 text-left mt-5">Las mejores resinas a tu disposición. Los productos más destacados de los fabricantes</p>
                     </v-col>
                     <v-col cols="12" lg="3" md="3" sm="12" xs="12">
                         <v-img src="/group/logo-adilene.png" contain max-height="90"></v-img>
-                        <p class="body-1 text-left mt-5">Aditivos para solventar cualquier reto. En Adilene encontrarás un amplio abanico de aditivos para todo tipo de aplicaciones.</p>
+                        <p class="font-size-18 text-left mt-5">Aditivos para solventar cualquier reto. En Adilene encontrarás un amplio abanico de aditivos para todo tipo de aplicaciones.</p>
                     </v-col>
                 </v-row>
             </v-container>
@@ -116,9 +116,5 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Archivo:wght@300&display=swap');
 
-.font-title{
-    font-family: 'Archivo' !important;
-}
 </style>
