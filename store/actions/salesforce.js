0aff32cd8d94fbaadcf82540a8bb2b0e9096b32a
aleDonatto <ale_donatto@yahoo.com.mx>
2022-07-27 15:22:13 -0500

    fixes store

--- store/actions/salesforce.js
+++ store/actions/salesforce.js
@@ -9,19 +9,20 @@ export default {
         params.append('client_secret', state.userSalesforce.client_secret)
 
         //await this.$axios.post('https://test.salesforce.com/Token', params)
-        await this.$axios.post('/salesforce/services/oauth2/token', params)
+        //await this.$axios.post('/salesforce/services/oauth2/token', params)
+        await this.$axios.post('/salesforce/services/oauth2/token?username=desatimx@polnac.com.bxt01&password=H1u2g3o435&grant_type=password&client_id=3MVG9Iu66FKeHhINolE5zsI1ShisdkRJu7w4dK2dqtwu4ye5nOA70PgSM_M9u58zLzERvwfmhdYWyt2pnyFxh&client_secret=C60E440505B55C377DB5B52E7FFF0BDB453E391F11F5642DBE01ACB680969F8F')
         .then(res => {
             let access_token = res.data.access_token
             
             this.$axios.setToken(access_token, 'Bearer')
-            this.axios.defaults.headers.common = {'Authorization': `Bearer ${access_token}`}
+            //this.axios.defaults.headers.common = {'Authorization': `Bearer ${access_token}`}
 
             localStorage.setItem('userToken', access_token)
             commit('StateAssign', {credentials: res.data})
-            console.log('get credentiales')
+            console.log('get credentials')
         })
         .catch( err => {
-            console.log('error credentiales ' + err)
+            console.log('getToken(): ' + err)
         })
     }, 
 
