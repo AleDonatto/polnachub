e653b60301106b2dc372c5e52764081fed9df485
aleDonatto <ale_donatto@yahoo.com.mx>
2022-08-12 13:04:27 -0500

    fixes marcas

--- components/Grupo/Marcas.vue
+++ components/Grupo/Marcas.vue
@@ -4,16 +4,16 @@
             <v-container>
                 <v-row justify="center" class="mt-16">
                     <v-col cols="12" align="center">
-                        <h1 class="font-archivo font-size-30 font-weight-bold">Rototech by POLNAC</h1>
+                        <h1 class="font-archivo font-size-30"><span class="font-weight-black">Rototech by POLNAC</span></h1>
                     </v-col>
                 </v-row>
 
-                <v-row justify="center">
-                    <v-col cols="12" lg="6" md="6" sm="12" xs="12" align-self="center">
+                <v-row justify="center" :class="{'mx-2': windowSize<1129 }" >
+                    <v-col cols="12" xl="5" lg="6" md="6" sm="11" xs="12" align-self="center">
                         <v-img src="/group/roboteach.png" contain max-height="280"></v-img>
                     </v-col>
-                    <v-col cols="12" lg="6" md="6" sm="12" xs="12">
-                        <p class="text-body-all">
+                    <v-col cols="12" xl="4" lg="6" md="6" sm="12" xs="12">
+                        <p class="" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">
                             <span class="font-weight-bold">La línea de negocio especializada en la producción y comercialización de resinas plásticas para el proceso de rotomoldeo.</span>
                             <br><br>
                             Productos de la más alta calidad y diseñados de acuerdo a tus especificaciones. Rototech brinda resultados y soluciones eficientes sin importar la aplicación final.
@@ -24,7 +24,7 @@
                 </v-row>
 
                 <v-row justify="center">
-                    <v-col cols="12" class="my-16">
+                    <v-col cols="12" xl="9" class="my-16">
                         <v-carousel class="carousel-black" height="340" cycle hide-delimiter-background show-arrows-on-hover v-if="windowSize > 1129">
                             <v-carousel-item v-for="(item,i) in 4" :key="i">
                                 <v-sheet height="100%">
@@ -45,7 +45,7 @@
 
                         <v-carousel class="carousel-black" height="340" cycle hide-delimiter-background show-arrows-on-hover v-if="windowSize < 1129">
                             <v-carousel-item v-for="(item,i) in items" :key="i">
-                                <v-sheet color="white" height="100%">
+                                <v-sheet color="white" height="95%">
                                     <v-row>
                                         <v-col cols="12">
                                             <v-img :src="item.img" contain max-height="270"></v-img>
@@ -57,65 +57,79 @@
                     </v-col>
                 </v-row>
 
-                <v-row justify="center" class="mb-10">
-                    <v-col cols="12" lg="6" md="6" sm="12" xs="12">
-                        <p class="font-weight-bold text-body-all">Dentro de toda la gama de productos Rototech encontrarás: </p>
+                <v-row justify="center" class="mb-10" :class="{'mx-2': windowSize<1129 }">
+                    <v-col cols="12" xl="5" lg="6" md="6" sm="12" xs="12">
+                        <p class="font-weight-bold" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">Dentro de toda la gama de productos Rototech encontrarás: </p>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/check.png" height="17" alt="">
-                            <p class=" ml-3 text-body-all">Certificación FDA, aprobados para estar en contacto con alimentos y bebidas.</p>
+                            <p class=" ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">
+                                Certificación FDA, aprobados para estar en contacto con alimentos y bebidas.
+                            </p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/check.png" height="17" alt="">
-                            <p class=" ml-3 text-body-all">UL 94 certificación en flamabilidad.</p>
+                            <p class=" ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">
+                                {{ windowSize > 1129 ? 'UL 94 certificación en flamabilidad.' : 'Tamaño de partícula controlada.' }}
+                            </p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/check.png" height="17" alt="">
-                            <p class=" ml-3 text-body-all">Tamaño de partícula controlada mesh 35 regulada ARM.</p>
+                            <p class=" ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">
+                                {{ windowSize>1129 ? 'Tamaño de partícula controlada mesh 35 regulada ARM.': 'Excelentes propiedades mecánicas: estupenda estabilidad térmica, alta resistencia al impacto e intemperismo.'}}
+                            </p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/check.png" height="17" alt="">
-                            <p class=" ml-3 text-body-all">Excelentes propiedades mecánicas: estupenda estabilidad térmica, alta resistencia al impacto y resistencia a la intemperie (V16).</p>
+                            <p class=" ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">
+                                {{ windowSize>1129 ? 'Excelentes propiedades mecánicas: estupenda estabilidad térmica, alta resistencia al impacto y resistencia a la intemperie (V16).' : 'Protección a luz ultravioleta.' }}
+                            </p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/check.png" height="17" alt="">
-                            <p class=" ml-3 text-body-all">Libres de cualquier tipo de contaminación (metales pesados).</p>
+                            <p class=" ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">
+                                Libres de cualquier tipo de contaminación (metales pesados).
+                            </p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/check.png" height="17" alt="">
-                            <p class=" ml-3 text-body-all">Gran variedad de colores.</p>
+                            <p class=" ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">
+                                {{ windowSize> 1129 ? 'Gran variedad de colores.': 'Gran variedad de colores, incluyendo negro, natural, negro de alta calidad, entre otros.'}}
+                            </p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/check.png" height="17" alt="">
-                            <p class=" ml-3 text-body-all">Compuestos especiales: espumados, granito, y antibacterial.</p>
+                            <p class=" ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">
+                                Compuestos especiales: espumados, granito, y antibacterial.
+                            </p>
                         </div>
                     </v-col>
-                    <v-col cols="12" lg="6" md="6" sm="12" xs="12">
-                        <p class="font-weight-bold text-body-all">Mercados:</p>
+                    <v-col cols="12" xl="4" lg="6" md="6" sm="12" xs="12">
+                        <p class="font-weight-bold" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">Mercados:</p>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/lconstruccion.png" height="22" alt="">
-                            <p class="ml-3 text-body-all">Construcción</p>
+                            <p class="ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">Construcción</p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/lconsumo.png" height="22" alt="">
-                            <p class="ml-3 text-body-all">Consumo</p>
+                            <p class="ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">Consumo</p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/lindustrial.png" height="22" alt="">
-                            <p class="ml-3 text-body-all">Industrial</p>
+                            <p class="ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">Industrial</p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/lautomotriz.png" height="22" alt="">
-                            <p class="ml-3 text-body-all">Automotriz y Transportación</p>
+                            <p class="ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">Automotriz y Transportación</p>
                         </div>
                         <div class="d-flex inline-block">
                             <img src="../../static/group/lagricola.png" height="22" alt="">
-                            <p class="ml-3 text-body-all">Agrícola</p>
+                            <p class="ml-3" :class="{'font-size-20': windowSize>1129, 'text-body-all': windowSize<1129 }">Agrícola</p>
                         </div>
                     </v-col>
                 </v-row>
 
                 <v-row class="mt-5" justify="center">
-                    <v-col cols="12" class="bg-catalogo rounded-lg mb-10" align="center">
+                    <v-col cols="12" xl="9" class="bg-catalogo rounded-lg mb-10" align="center">
                         <h1 class="font-archivo white--text font-weight-bold font-size-34 mt-10">¿Deseas consultar nuestro catálogo?</h1>
 
                         <nuxt-link to="/search-products" class="decoration-none">
@@ -127,38 +141,38 @@
                 </v-row>
 
                 <v-row justify="center" class="mt-16">
-                    <v-col cols="8">
+                    <v-col cols="8" xl="5">
                         <h1 class="font-size-30 font-weight-bold">Otras marcas</h1>
                     </v-col>
-                    <v-col cols="4" align="right">
+                    <v-col cols="4" xl="4" align="right">
                         <nuxt-link to="/polnac-group" class="color-decoration">
                             <h1 class="purple-polnac" >Ver todas</h1>
                         </nuxt-link>
                     </v-col>
                 </v-row>
                 <v-row justify="center" class="mb-16" v-if="windowSize > 1129">
-                    <v-col cols="3" align="center">
+                    <v-col cols="3" xl="2" align="center">
                         <nuxt-link to="/marcas/1" class="decoration-none">
                             <v-img src="/group/logo-bioresin.png" contain max-width="210"></v-img>
-                            <p class="text-body-all text-left mt-2 black--text">Especialistas en la distribución y fabricación de resinas biodegradables. </p>
+                            <p class="text-body-all text-left mt-2 black--text text-truncate">Especialistas en la distribución y fabricación de resinas biodegradables. </p>
                         </nuxt-link>
                     </v-col>
-                    <v-col cols="3" align="center">
+                    <v-col cols="3" xl="2" align="center">
                         <nuxt-link to="/marcas/2" class="decoration-none">
                             <v-img src="/group/logo-flexlene.png" contain max-width="210"></v-img>
-                            <p class="text-body-all text-left mt-2 black--text">Expertos en el diseño, desarrollo y fabricación de Compuestos de Policloruro de Vinilo (PVC).</p>
+                            <p class="text-body-all text-left mt-2 black--text text-truncate">Expertos en el diseño, desarrollo y fabricación de Compuestos de Policloruro de Vinilo (PVC).</p>
                         </nuxt-link>
                     </v-col>
-                    <v-col cols="3" align="center">
+                    <v-col cols="3" xl="2" align="center">
                         <nuxt-link to="/marcas/3" class="decoration-none">
                             <v-img src="/group/logo-politene.png" contain max-width="210"></v-img>
-                            <p class="text-body-all text-left mt-2 black--text">Las mejores resinas a tu disposición. Los productos más destacados de los fabricantes</p>
+                            <p class="text-body-all text-left mt-2 black--text text-truncate">Las mejores resinas a tu disposición. Los productos más destacados de los fabricantes</p>
                         </nuxt-link>
                     </v-col>
-                    <v-col cols="3" align="center">
+                    <v-col cols="3" xl="2" align="center">
                         <nuxt-link to="/marcas/4" class="decoration-none">
                             <v-img src="/group/logo-adilene.png" contain max-width="210"></v-img>
-                            <p class="text-body-all text-left mt-2 black--text">Aditivos para solventar cualquier reto. En Adilene encontrarás un amplio abanico de aditivos para todo tipo de aplicaciones.</p>
+                            <p class="text-body-all text-left mt-2 black--text text-truncate">Aditivos para solventar cualquier reto. En Adilene encontrarás un amplio abanico de aditivos para todo tipo de aplicaciones.</p>
                         </nuxt-link>
                     </v-col>
                 </v-row>
